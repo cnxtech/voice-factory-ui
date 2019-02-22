add99b067ad9bc2ffe2856309d69d0b90a307878
wirsing <me@wilmthoben.com>
2019-02-21 18:32:20 -0800

    Initial commit.

---
+++ src/components/Speech.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  .nfl-voice-experiment
+    speech-to-text(@onEnd="onEnd" @onRunning="onRunning")
+    .query
+      h1 {{ currentTranscription }}
+    .content(v-if="result")
+      h1 Intent: {{ intent }}
+      h1 Entity: {{ entity }}
+      h1 Answer: {{ answer }}
+</template>
+
+<script>
+import axios from 'axios'
+import SpeechToText from './SpeechToText.vue'
+
+export default {
+  name: 'HelloWorld',
+
+  components: {
+    SpeechToText
+  },
+
+  data() {
+    return {
+      result: false,
+      currentTranscription: ''
+    }
+  },
+
+  computed: {
+    intent() {
+      return this.valid ? this.result.responseIntentName : false
+    },
+
+    entity() {
+      return this.valid ? this.result.alignedVariables.FootballTerm : false
+    },
+
+    answer() {
+      return this.valid ? this.result.fulfillmentResponses[0].fulfillment : false
+    },
+
+    valid() {
+      return this.result && this.result.responseType === 'FULFILLED'
+    }
+  },
+
+  methods: {
+    onEnd(transcription) {
+      const query = {
+        questionProperties: {
+          naturalLanguageQuery: transcription
+        }
+      }
+      this.query(query)
+      this.result = false
+    },
+
+    onRunning(transcription) {
+      this.currentTranscription = transcription
+    },
+
+    query(query) {
+      axios.post('http://18.235.66.33:8099/v1/ask', query)
+      .then( response => {
+        console.log(response.data)
+        this.result = response.data.answerProperties
+      })
+      .catch( error => {
+        console.log(error)
+      });
+    }
+  }
+}
+</script>
+
+
+<style lang="sass">
+
+</style>
---
+++ src/components/SpeechToText.vue
@@ -0,0 +1,65 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'SpeechToText',
+
+  props: {
+    lang: {
+      type: String,
+      default: 'en-US'
+    }
+  },
+
+  data() {
+    return {
+      runtimeTranscription: '',
+      transcription: '',
+      recognition: null
+    }
+  },
+
+  methods: {
+    initApi () {
+      window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+      if (!SpeechRecognition && process.env.NODE_ENV !== 'production') {
+        throw new Error('Speech Recognition does not exist on this browser. Use Chrome or Firefox')
+      }
+
+      if (!SpeechRecognition) {
+        return
+      }
+
+      this.recognition = new SpeechRecognition()
+      this.recognition.lang = this.lang
+      this.recognition.interimResults = true
+
+      this.recognition.addEventListener('result', event => {
+        const text = Array.from(event.results)
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join('')
+        this.runtimeTranscription = text
+        this.$emit('onRunning', this.runtimeTranscription)
+      })
+
+      this.recognition.addEventListener('end', () => {
+        if (this.runtimeTranscription !== '') {
+          this.transcription = this.runtimeTranscription
+          this.$emit('onEnd', this.transcription)
+        }
+        this.runtimeTranscription = ''
+        this.recognition.start()
+      })
+
+      this.recognition.start()
+    }
+  },
+  mounted () {
+    this.initApi()
+  }
+}
+</script>
